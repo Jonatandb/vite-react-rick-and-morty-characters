6367a53760c97989684b20e661046b49f0c45445
Jonatandb <jonatandb@gmail.com>
2022-06-26 00:12:45 -0300

    Added configuration to deploy to Github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: 'https://jonatandb.github.io/Jonatandb/vite-react-rick-and-morty-characters',
+  plugins: [react()],
 })
+
