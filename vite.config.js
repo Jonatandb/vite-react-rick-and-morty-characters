072e1a8437c9c1de171a2f0fdad3a910b5fcabd5
Jonatandb <jonatandb@gmail.com>
2022-06-26 00:33:56 -0300

    Vite configuration updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://jonatandb.github.io/Jonatandb/vite-react-rick-and-morty-characters',
+  base: 'https://jonatandb.github.io/vite-react-rick-and-morty-characters',
   plugins: [react()],
 })
 
