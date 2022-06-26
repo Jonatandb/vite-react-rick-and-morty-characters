809e75cdfe1cc7ce747e565c20731929806defc9
Jonatandb <jonatandb@gmail.com>
2022-06-25 23:00:02 -0300

    Fetching of characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
-  return <h1>Jonatandb</h1>
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    let mounted = true
+    async function fetchData() {
+      const response = await fetch(
+        'https://rickandmortyapi.com/api/character?page=1',
+      )
+      const data = await response.json()
+      console.log(mounted)
+      if (mounted) {
+        setCharacters(data.results)
+      }
+    }
+    fetchData()
+
+    return () => (mounted = false)
+  }, [])
+
+  return (
+    <>
+      <h1>Vite/React - Rick and Morty characters</h1>
+      {characters.map(character => (
+        <div key={character.id}>
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+        </div>
+      ))}
+    </>
+  )
 }
 
